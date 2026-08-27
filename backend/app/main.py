7866f1945f9a2dfc9e18662da8bd48da516e0858
pknowledge <harshselokar07@gmail.com>
2026-08-27 17:18:08 +0530

    fix production frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+	"https://social-media-ai-frotend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
